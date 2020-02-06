d0b946510b8069c7bb2f02f5bb87fb9b684f6934
angryviking <michael.hanna.ctr@isso.net>
2020-02-06 15:25:00 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,216 @@
+var config = {
+    style: 'mapbox://styles/hannaman72/ck62komab0pox1irsbaxord1x',
+    accessToken: 'pk.eyJ1IjoiaGFubmFtYW43MiIsImEiOiJjamdnajQ2NTMzMm1pMnFtcTdyem1iemFoIn0.ix2-5RYo-E_pDqDl7pI3KA',
+    showMarkers: false,
+    theme: 'light',
+    alignment: 'left',
+    title: '<a href="mailto:mhanna72@gmail.com?subject=Resume Interest">Michael Hanna</a>',
+    subtitle: 'Linux Engineer',
+    byline: 'Industry certified Linux engineer with over 20 years experience working in DoD cleared environments.',
+    footer: '<div style="display:flex;justify-content:space-around;width:100%;padding:0;"> \
+                 <div><img src="images/ece.png" width="100" height="100" alt="Elasticsearch Engineer"></div> \
+                 <div><img src="images/rhce.png" width="200" height="100" alt="Red Hat Engineer"></div> \
+                 <div><img src="images/rhcsa.png" width="200" height="100" alt="Red Hat System Adminsitrator"></div> \
+                 <div><img src="images/ansible.png" width="200" height="100" alt="Ansible Specialist"></div> \
+             </div>',
+    chapters: [
+        {
+            id: 'intro',
+            title: '',
+            image: '',
+            description: '<h4>Skills</h4> \
+                          <div style="display:inline-block;width:100%;padding:0;"> \
+                              <div style="padding:0;float:left;width:75%;"> \
+                                  <progress style="width:100%;" value="80" max="100"></progress> \
+                              </div> \
+                              <div style="padding:0;float:right;">Linux</div> \
+                          </div> \
+                          <div style="display:inline-block;width:100%;padding:0;"> \
+                              <div style="padding:0;float:left;width:75%;"> \
+                                  <progress style="width:100%;" value="80" max="100"></progress> \
+                              </div> \
+                              <div style="padding:0;float:right;">Shell Scripting</div> \
+                          </div> \
+                          <div style="display:inline-block;width:100%;padding:0;"> \
+                              <div style="padding:0;float:left;width:75%;"> \
+                                  <progress style="width:100%;" value="75" max="100"></progress> \
+                              </div> \
+                              <div style="padding:0;float:right;">Automation</div> \
+                          </div> \
+                          <div style="display:inline-block;width:100%;padding:0;"> \
+                              <div style="padding:0;float:left;width:75%;"> \
+                                  <progress style="width:100%;" value="75" max="100"></progress> \
+                              </div> \
+                              <div style="padding:0;float:right;">Docker</div> \
+                          </div> \
+                          <div style="display:inline-block;width:100%;padding:0;"> \
+                              <div style="padding:0;float:left;width:75%;"> \
+                                  <progress style="width:100%;" value="60" max="100"></progress> \
+                              </div> \
+                              <div style="padding:0;float:right;">VMWare</div> \
+                          </div> \
+                          <div style="display:inline-block;width:100%;padding:0;"> \
+                              <div style="padding:0;float:left;width:75%;"> \
+                                  <progress style="width:100%;" value="50" max="100"></progress> \
+                              </div> \
+                              <div style="padding:0;float:right;">AWS</div> \
+                          </div> \
+                          <div style="display:inline-block;width:100%;padding:0;"> \
+                              <div style="padding:0;float:left;width:75%;"> \
+                                  <progress style="width:100%;" value="30" max="100"></progress> \
+                              </div> \
+                              <div style="padding:0;float:right;">Kubernetes</div> \
+                          </div> \
+                          <h4>Certifications</h4> \
+                          <div style="display:inline-block;width:100%;padding:0;"> \
+                              <div style="padding:0;">Elasticsearch Certified Engineer</div> \
+                              <div style="padding:0;"> \
+                                  <a target="_blank" href="https://www.redhat.com/rhtapps/services/certifications/badge/verify/2H4ASSBQPCX73FHQQTCMOHBWX4AEQU3CUPSQX2KSDXT6RW46LQ34UFHA6EGV4MX6OEQWWNEDUIWXWPUWTPNOZCAXTQD32BJ2PLFPHS3STVWDCMJUD3KGSZYJTPS2YGTCOKOWYMJRGQPNI2LHBGN6LLA2MI======"> \
+                                      Red Hat Certified Specialist in Ansible Automation \
+                                  </a></div> \
+                              <div style="padding:0;"> \
+                                  <a target="_blank" href="https://www.redhat.com/rhtapps/services/certifications/badge/verify/2H4ASSBQPCX73FHQQTCMOHBWX4AEQU3CUPSQX2KSDXT6RW46LQ3XCZJWRJNV7ILTXVE4I6VB7OTCG4U5NQYTCNA62RUWOCM34WWBUYQ="> \
+                                      Red Hat Certified Engineer \
+                                  </a></div> \
+                              <div style="padding:0;"> \
+                                  <a target="_blank" href="https://www.redhat.com/rhtapps/services/certifications/badge/verify/2H4ASSBQPCX73FHQQTCMOHBWX4AEQU3CUPSQX2KSDXT6RW46LQ3T7ULZ55KZZ56SKO7EQ3ETTLYZQ4U5NQYTCNA62RUWOCM34WWBUYQ="> \
+                                      Red Hat Certified System Administrator \
+                                  </a></div> \
+                              <div style="padding:0;">CompTIA Security + ce</div> \
+                              <div style="padding:0;">CompTIA Linux +</div> \
+                          </div> \
+                          <h4>Degrees</h4> \
+                          <div style="display:inline-block;width:100%;padding:0;"> \
+                              <div style="padding:0;float:left;">Master of Business Adminsistration</div> \
+                              <div style="padding:0;float:right;">Saint Leo University</div> \
+                          </div> \
+                          <div style="display:inline-block;width:100%;padding:0;"> \
+                              <div style="padding:0;float:left;">Bachelor of Science in Computer Information Systems</div> \
+                              <div style="padding:0;float:right;">Saint Leo University</div> \
+                          </div>',
+            location: {
+		        center: [-99.87789, 40.15596],
+		        zoom: 3.18,
+		        pitch: 0.00,
+		        bearing: 0.00
+            },
+            onChapterEnter: [
+                {
+                    layer: 'poi-label',
+                    opacity: 0
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'poi-label',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'novetta',
+            title: '<div style="display:flex;justify-content:space-between;width:100%;padding:0;"> \
+                        <div style="padding:0;">Novetta</div> \
+                        <div style="padding:0;">Tampa, FL</div> \
+                        <div style="padding:0;">04/2017 - Present</div> \
+                    </div>',
+            image: 'images/novetta.png',
+            description: '<h5>Senior Linux Engineer</h5> \
+                          <li>Linux engineer for a Department of the Air Force research and development activity</li> \
+                          <li>Deployed of over 200 CentOS and Red Hat Linux servers both on-premises and in cloud environments</li> \
+                          <li>Managed server instances using Spacewalk, Ansible, and Terraform</li> \
+                          <li>Wrote many shell scripts to help with administration of the infrastructure</li> \
+                          <li>Maintained security posture of Linux infrastructure, including deploying STIGs, installing virus scanners, and running SCAP scans</li> \
+                          <li>Engineered, deployed, and managed several big data Elasticsearch clusters on different networks</li> \
+                          <li>Built Docker containers and deployed containers in Docker swarms on multiple networks</li> \
+                          <li>Migrated systems and images between VirtualBox, VMWare, and AWS</li> \
+                          <li>Researched new technologies and solutions to interesting data problems</li>',
+            location: {
+		        center: [-82.45704, 27.95081],
+		        zoom: 16.00,
+		        pitch: 29.50,
+		        bearing: 0.00
+            },
+            onChapterEnter: [
+                {
+                    layer: 'poi-label',
+                    opacity: 0
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'poi-label',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'booz-allen-hamilton',
+            title: '<div style="display:flex;justify-content:space-between;width:100%;padding:0;"> \
+                        <div style="padding:0;">Booz Allen Hamilton</div> \
+                        <div style="padding:0;">MacDill AFB, FL</div> \
+                        <div style="padding:0;">10/2016 - 04/2017</div> \
+                    </div>',
+            image: 'images/boozallenhamilton.svg',
+            description: '<h5>Senior Systems Engineer</h5> \
+                          <li>Linux and VMWare engineer responsible for operation and support of many virtual and physical servers in a large globally distributed enterprise</li> \
+                          <li>Created and maintained scripts, documentation and processes to ensure smooth deployment and management of VMWare virtual machines</li> \
+                          <li>Managed an Enterprise Infoblox DNS appliance</li> \
+                          <li>Built and supported a distributed ISSE Guard cross domain solution while implementing innovative solutions</li>',
+            location: {
+		        center: [-82.48473,27.85203],
+		        zoom: 15.76,
+		        pitch: 60.00,
+		        bearing: -133.60
+            },
+            onChapterEnter: [
+                {
+                    layer: 'poi-label',
+                    opacity: 0
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'poi-label',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'bae-systems',
+            title: '<div style="display:flex;justify-content:space-between;width:100%;padding:0;"> \
+                        <div style="padding:0;">BAE Systems</div> \
+                        <div style="padding:0;">Stuttgart, Germany/MacDill AFB, FL</div> \
+                        <div style="padding:0;">10/2011 - 10/2016</div> \
+                    </div>',
+            image: 'images/baesystems.svg',
+            description: '<h5>Senior Systems Engineer</h5> \
+                          <li>UNIX systems administrator responsible for the DoDIIS Trusted Workstation suite</li> \
+                          <li>Managed various intelligence applications running on Solaris platforms, such as M3</li> \
+                          <li>Responsible for building and fielding new Solaris based servers to replace legacy hardware</li> \
+                          <li>Managed NIS and NIS+ naming services</li> \
+                          <li>Performed DNS migration from legacy UNIX platform to Enterprise Infoblox solution</li> \
+                          <li>Built servers using HP Server Administration System</li> \
+                          <li>Installed configured and managed LAMP and FTP servers</li> \
+                          <li>Wrote shell and perl scripts to automatically maintain servers and allow the help desk to provide greater customer service</li>',
+            location: {
+		        center: { lon: 9.08146, lat: 48.73666 },
+		        zoom: 16.00,
+		        pitch: 55.00,
+		        bearing: 26.40
+            },
+            onChapterEnter: [
+                {
+                    layer: 'poi-label',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'poi-label',
+                    opacity: 0
+                }
+            ]
+        }
+    ]
+};
